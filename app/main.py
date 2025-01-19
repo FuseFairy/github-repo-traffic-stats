c75a1aa56c8e34f402f50c29bd532844fd59fc70
FuseFairy <dd8611706@gmail.com>
2025-01-19 19:08:48 +0800

    feat: store svg in cache

--- app/main.py
+++ app/main.py
@@ -1,23 +1,25 @@
 from contextlib import asynccontextmanager
-import traceback
 from fastapi import FastAPI, Query, Response, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from cachetools import TTLCache
 from app.services.github_api import get_all_traffic_data, get_profile_name
 from app.services.chart_generator import generate_chart
 from dotenv import load_dotenv, find_dotenv
-import hashlib
 from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
 
 load_dotenv(find_dotenv())
 
 cache = TTLCache(maxsize=10, ttl=float('inf'))
+chart_cache = TTLCache(maxsize=10, ttl=float('inf'))
+task_last_called = {}
 
 # Background Scheduler setup
 scheduler = BackgroundScheduler()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    scheduler.add_job(check_and_remove_task, 'interval', days=8, id=f"check_task")
     scheduler.start()
     # print("Scheduler started.")
     try:
@@ -27,11 +29,6 @@ async def lifespan(app: FastAPI):
         scheduler.shutdown()
 
 app = FastAPI(lifespan=lifespan)
-
-def schedule_update_data(username, traffic_results_key, profile_name_key):
-    # print("Scheduling periodic data update...")
-    scheduler.add_job(generate_new_data, 'interval', minutes=29, id=username,
-                      args=[username, traffic_results_key, profile_name_key], replace_existing=True)
 
 @app.get("/")
 def root():
@@ -73,8 +70,7 @@ def get_traffic_chart(
         A response containing the chart in SVG format.
     """
     try:
-        # Get If-None-Match header (client's ETag)
-        if_none_match = request.headers.get("If-None-Match")
+        chart_cache_key = f"{username}_{theme}_{bg_color}_{clones_color}_{views_color}_{clones_point_color}_{views_point_color}_{radius}_{height}_{width}_{exclude_repos}"
         
         # Check if traffic data is already cached
         traffic_results_key = f"traffic_data_{username}"
@@ -83,29 +79,28 @@ def get_traffic_chart(
         # Get or generate data
         if traffic_results_key not in cache and profile_name_key not in cache:
             generate_new_data(username, traffic_results_key, profile_name_key)
-            schedule_update_data(username, traffic_results_key, profile_name_key)
+            scheduler.add_job(generate_new_data, 'interval', minutes=28, id=username,
+                      args=[username, traffic_results_key, profile_name_key], replace_existing=True)
 
-        traffic_results = cache[traffic_results_key]
-        profile_name = cache[profile_name_key]
+        if chart_cache_key not in chart_cache:
+            traffic_results = cache[traffic_results_key]
+            profile_name = cache[profile_name_key]
 
-        # Generate chart
-        chart_svg = generate_chart(profile_name, traffic_results, theme, height, width, radius, bg_color,
-                                   clones_color, views_color, clones_point_color, views_point_color, exclude_repos)
+            # Generate chart
+            chart_cache[chart_cache_key] = generate_chart(profile_name, traffic_results, theme, height, width, radius, bg_color, 
+                                                          clones_color, views_color, clones_point_color, views_point_color, exclude_repos)
+            scheduler.add_job(generate_chart, 'interval', minutes=29, id=chart_cache_key, args=[profile_name, traffic_results, theme, height, width, radius, bg_color, 
+                                                          clones_color, views_color, clones_point_color, views_point_color, exclude_repos], replace_existing=True)
         
-        # Generate ETag
-        chart_hash = hashlib.md5(chart_svg.encode("utf-8")).hexdigest()
-        
-        # If ETag matches, return 304 Not Modified
-        if if_none_match and if_none_match == chart_hash:
-            return Response(status_code=304)
+        chart_svg = chart_cache[chart_cache_key]
+        task_last_called[chart_cache_key] = datetime.now()
 
         # Set headers
         headers = {
             "Content-Type": "image/svg+xml; charset=utf-8",
-            "Cache-Control": "public, max-age=1800, must-revalidate",
-            "ETag": chart_hash
+            "Cache-Control": "public, max-age=1800",
         }
-
+        
         return Response(
             content=chart_svg,
             media_type="image/svg+xml",
@@ -116,6 +111,23 @@ def get_traffic_chart(
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:        
         raise HTTPException(status_code=500, detail=str(e))
+
+def check_and_remove_task():
+    # Check all chart cache keys for expired tasks
+    now = datetime.now()
+    
+    for chart_cache_key, last_called in task_last_called.items():
+        # If the task has not been accessed for more than 2 days
+        if now - last_called > timedelta(weeks=1):
+            # Remove the job associated with the chart cache key
+            scheduler.remove_job(chart_cache_key)
+            
+            # Delete the entry from task_last_called
+            del task_last_called[chart_cache_key]
+            
+            # Also remove the chart from the cache if it exists
+            if chart_cache_key in chart_cache:
+                del chart_cache[chart_cache_key]
 
 # Function to generate new data
 def generate_new_data(username, traffic_results_key, profile_name_key):
