28c1ffc585ebcc465999d6fe77bb5abea34e5c27
FuseFairy <dd8611706@gmail.com>
2025-01-20 08:12:22 +0800

    update Cache-Control

--- app/main.py
+++ app/main.py
@@ -113,7 +113,7 @@ async def get_traffic_chart(
         # Set headers
         headers = {
             "Content-Type": "image/svg+xml; charset=utf-8",
-            "Cache-Control": "public, max-age=1800, must-revalidate",
+            "Cache-Control": "public, max-age=1800, s-maxage=3600, stale-while-revalidate=86400"
         }
 
         return Response(
