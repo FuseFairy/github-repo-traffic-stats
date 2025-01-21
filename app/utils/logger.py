ce686a71b64272f56987c626dce31cf825091b9b
FuseFairy <dd8611706@gmail.com>
2025-01-21 15:39:41 +0800

    refactor: remove app.log

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -6,16 +6,11 @@ logger.setLevel(logging.DEBUG)
 
 if not logger.hasHandlers():
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
-
-    file_handler = logging.FileHandler("app.log")
-    file_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
         "[%(asctime)s] [%(levelname)s] [%(name)s] - %(message)s"
     )
     console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
